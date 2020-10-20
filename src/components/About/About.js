c3d91d20b31481a78f77de2a5de9aab5e9247cc0
sskywalker <hkrai39@gmail.com>
2020-10-20 23:02:46 +0530

    corrected about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,17 +1,22 @@
 import React from "react"
 import "./about.styles.css"
-const Header = () => (
-  <header>
-    <div className="about-wrapper">
-      <h1>About Me</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-        aperiam modi cum, sequi natus expedita cumque ducimus dolorum dolore
-        eaque doloremque numquam. Ratione eius quos cum dolore quam quidem
-        obcaecati.
-      </p>
-    </div>
-  </header>
+const About = () => (
+  <div className="about-wrapper">
+    <h1>About Me</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident aperiam
+      modi cum, sequi natus expedita cumque ducimus dolorum dolore eaque
+      doloremque numquam. Ratione eius quos cum dolore quam quidem obcaecati.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis
+      debitis dolorum quis porro soluta aut incidunt repudiandae ea, laborum
+      quod nisi aliquam odit eligendi harum labore, id dolorem in officia!
+    </p>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius natus
+      debitis eaque nam quas ullam mollitia fugit quisquam ipsum libero.
+    </p>
+  </div>
 )
 
-export default Header
+export default About
