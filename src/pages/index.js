125ff4a5ea168c8d2a20350c65756385e46df1e0
sskywalker <hkrai39@gmail.com>
2020-10-20 21:41:59 +0530

    added basic skeleton layout of home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,10 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import GithubLogo from "../assets/github.svg"
+import InstagramLogo from "../assets/instagram.svg"
+import TwitterLogo from "../assets/twitter.svg"
+
 import "../components/index.styles.css"
 
 const IndexPage = () => (
@@ -10,7 +14,14 @@ const IndexPage = () => (
     <SEO title="Home" />
     <div className="home-container">
       <div className="intro">
-        <div className="socials"></div>
+        <div className="socials">
+          <div className="line"></div>
+          <div className="social-icons">
+            <GithubLogo />
+            <InstagramLogo />
+            <TwitterLogo />
+          </div>
+        </div>
         <h1>Hi, I'm Hrishikesh .</h1>
         <h1>Front end developer,</h1>
         <h1>CS graduate and javascript enthusiast.</h1>
