c870acf73ee24aed4671e6f9139c84d379052984
sskywalker <hkrai39@gmail.com>
2020-10-20 22:46:20 +0530

    added about me page

---
+++ src/components/About/About.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./about.styles.css"
+const Header = () => (
+  <header>
+    <div className="about-wrapper">
+      <h1>About Me</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+        aperiam modi cum, sequi natus expedita cumque ducimus dolorum dolore
+        eaque doloremque numquam. Ratione eius quos cum dolore quam quidem
+        obcaecati.
+      </p>
+    </div>
+  </header>
+)
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import About from "../components/About/About.js"
 
 import GithubLogo from "../assets/github.svg"
 import InstagramLogo from "../assets/instagram.svg"
@@ -13,25 +14,28 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <div className="home-container">
-      <div className="intro">
-        <div className="socials">
-          <div className="line"></div>
-          <div className="social-icons">
-            <GithubLogo />
-            <InstagramLogo />
-            <TwitterLogo />
+      <div className="intro-wrapper">
+        <div className="intro">
+          <div className="socials">
+            <div className="line"></div>
+            <div className="social-icons">
+              <GithubLogo />
+              <InstagramLogo />
+              <TwitterLogo />
+            </div>
+          </div>
+          <h2>Hi, I'm Hrishikesh.</h2>
+          <h2>Front end developer,</h2>
+          <h2>CS graduate and javascript enthusiast.</h2>
+          <div className="btn-group">
+            <button className="resume-btn view-resume-btn">View Resume</button>
+            <button className="resume-btn download-resume-btn">
+              Download Resume
+            </button>
           </div>
         </div>
-        <h2>Hi, I'm Hrishikesh.</h2>
-        <h2>Front end developer,</h2>
-        <h2>CS graduate and javascript enthusiast.</h2>
-        <div className="btn-group">
-          <button className="resume-btn view-resume-btn">View Resume</button>
-          <button className="resume-btn download-resume-btn">
-            Download Resume
-          </button>
-        </div>
       </div>
+      <About />
     </div>
   </Layout>
 )
