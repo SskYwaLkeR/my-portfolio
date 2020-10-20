7edde96caabd4c9038023e9ed1f9268d4e69f867
sskywalker <hkrai39@gmail.com>
2020-10-20 21:19:53 +0530

    added my basic intro

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,14 @@ import "../components/index.styles.css"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div className="home-container">Hello</div>
+    <div className="home-container">
+      <div className="intro">
+        <div className="socials"></div>
+        <h1>Hi, I'm Hrishikesh .</h1>
+        <h1>Front end developer,</h1>
+        <h1>CS graduate and javascript enthusiast.</h1>
+      </div>
+    </div>
   </Layout>
 )
 
