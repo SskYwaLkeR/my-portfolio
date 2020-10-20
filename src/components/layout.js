5f76e3d069d56adac28cd97cbeb7103dfea233a4
sskywalker <hkrai39@gmail.com>
2020-10-20 21:16:27 +0530

    adjusted width and height of DOM

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
@@ -24,27 +17,24 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
+    <div id="layout-wrapper">
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
+      <div className="layout-body">
+        <main
           style={{
-            marginTop: `2rem`,
+            width: `100%`,
+            height: `100%`,
           }}
         >
+          {children}
+        </main>
+        <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
       </div>
-    </>
+    </div>
   )
 }
 
