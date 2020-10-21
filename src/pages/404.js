033c43347d2dee22d2a0184bb9452caa3eccab67
sskywalker <hkrai39@gmail.com>
2020-10-21 21:21:58 +0530

    tweaked 404 message

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,8 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>404: Not Found 👻</h1>
+    <p>You just hit a page that I haven&#39;t build yet .</p>
   </Layout>
 )
 
