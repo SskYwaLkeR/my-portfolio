32cdf667e7f9302dda10fdfb030a9269fd4ed3f2
sskywalker <hkrai39@gmail.com>
2020-10-21 22:16:33 +0530

    deleted data.js

--- src/data.js
+++
@@ -1,6 +0,0 @@
-export const About = {
-  title: "About Me",
-  description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-   Id iste eos inventore vitae aliquid nobis. Temporibus deserunt, obcaecati
- delectus, aut eveniet voluptates assumenda ducimus expedita atque id laboriosam a ullam.`,
-}
