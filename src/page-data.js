aa84c66be1c08b035dd6ef5640d5868b22529605
sskywalker <hkrai39@gmail.com>
2020-10-21 23:19:37 +0530

    Added data file

---
+++ src/page-data.js
@@ -0,0 +1,61 @@
+export const AboutMe = {
+  title: `About Me`,
+  description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+     Eum, necessitatibus facere! Molestiae, eos. Iste, quis. Non corrupti nemo ut animi,
+     accusantium ratione incidunt porro velit earum laborum consequatur? Eius, obcaecati!`,
+}
+
+export const MySkills = {
+  title: `My Skills`,
+  description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    Eum, necessitatibus facere! Molestiae, eos. Iste, quis. Non corrupti nemo ut animi,
+    accusantium ratione incidunt porro velit earum laborum consequatur? Eius, obcaecati!`,
+}
+
+export const Experience = {
+  title: `Experience`,
+  description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    Eum, necessitatibus facere! Molestiae, eos. Iste, quis. Non corrupti nemo ut animi,
+    accusantium ratione incidunt porro velit earum laborum consequatur? Eius, obcaecati!`,
+}
+
+export const Projects = {
+  title: `Projects`,
+  description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Eum, necessitatibus facere! Molestiae, eos. Iste, quis. Non corrupti nemo ut animi,
+      accusantium ratione incidunt porro velit earum laborum consequatur? Eius, obcaecati!`,
+}
+
+export const LetsTalk = {
+  title: `Let'sTalk`,
+  description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      Eum, necessitatibus facere! Molestiae, eos. Iste, quis. Non corrupti nemo ut animi,
+      accusantium ratione incidunt porro velit earum laborum consequatur? Eius, obcaecati!`,
+}
+
+export const TimeLine = [
+  {
+    title: `We18.in`,
+    role: `Front-end developer`,
+    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+         Duis aute irure dolor in reprehenderit in voluptate.`,
+  },
+  {
+    title: `Tuutr.com`,
+    role: `Front-end developer`,
+    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+         Duis aute irure dolor in reprehenderit in voluptate.`,
+  },
+  {
+    title: `Still finiding`,
+    role: `javascript developer`,
+    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+         Duis aute irure dolor in reprehenderit in voluptate.`,
+  },
+]
