cc161601964b3e8e67761e9b69ec60c736d923b7
sskywalker <hkrai39@gmail.com>
2020-10-25 22:51:11 +0530

    added a new project adopt pets

--- src/project.data.js
+++ src/project.data.js
@@ -29,3 +29,29 @@ export const Ecomm = {
   techStackDescription: `I choose React because for an app like e-commerce, where user experience matters the most, React made more sense. SCSS was obvious choice
   because its syntactically similar to CSS and encourage proper nesting of rules. Heroku was chosed because of its reliability .   `,
 }
+
+export const AdoptPet = {
+  title: `Adopt animal`,
+  description: `Adopt animal is a react application where user can adopt pets. It was a follow along project when I was
+  learning the new React hooks concepts. I learnt a lot while building this project`,
+  projectStack: [
+    { stack: `react`, id: `s1` },
+    { stack: `SCSS`, id: `s2` },
+    { stack: `Heroku`, id: `s3` },
+  ],
+  images: {
+    HeroImg: HeroImg,
+    ImgOne: HeroImg,
+    ImgTwo: HeroImg,
+    ImgThree: HeroImg,
+  },
+  projGoal: `The motivation behind this app was to get good understanding of react concepts`,
+  challenges: `The First Challenge that I ran into was to structure the codebase. All the  components that I built, soon  became like pieces of puzzle and I realized
+  how important it is to structure the code base properly. Also as the development continued managing state of application became tougher and tougher
+  and to overcome this, I used redux .`,
+  lessonsLearned: `I can talk all day explaining all the lessons I learned while building this project, but the most important one was my newfound understanding
+  of Redux, codebase management, git, state management and using memoization to cache some data to improve performance of app `,
+
+  techStackDescription: `I choose React because for an app like e-commerce, where user experience matters the most, React made more sense. SCSS was obvious choice
+  because its syntactically similar to CSS and encourage proper nesting of rules. Heroku was chosed because of its reliability .   `,
+}
