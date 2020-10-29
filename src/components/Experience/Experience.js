9b1f7c57a15bbc5bd6e802a50966bd272938264b
sskywalker <hkrai39@gmail.com>
2020-10-30 00:44:03 +0530

    added styling

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -11,11 +11,16 @@ const Experience = () => (
       {TimeLine.map(data => (
         <div className={`timeline-block ${data.class}`} key={data.key}>
           <div className="marker"></div>
+
           <div className="timeline-content">
             <h2>{data.title}</h2>
             <span>{data.role}</span>
             <p>{data.description}</p>
-            {data.url.length ? <Link to={data.url}> Read story</Link> : null}
+            {data.url.length ? (
+              <Link to={data.url}>
+                <b>Read story</b>
+              </Link>
+            ) : null}
           </div>
         </div>
       ))}
