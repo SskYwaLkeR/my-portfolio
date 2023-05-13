1822966c5d625426409c6d840b3f33cade04e0c4
Hrishikesh Rai <hkrai39@gmail.com>
2023-05-13 23:18:24 +0530

    Update resume url

--- src/pages/index.js
+++ src/pages/index.js
@@ -102,7 +102,7 @@ const IndexPage = () => {
             <h2>CS graduate and javascript enthusiast.</h2>
             <div className="btn-group">
               <a
-                href="https://drive.google.com/file/d/1Y2OaKsNSWoy9xJHtxqcEgkkd_QH1MvIX/view?usp=sharing"
+                href="https://drive.google.com/file/d/1fMD3As2de6bMGlii1YbZOD5dTRnuByf_/view?usp=sharing"
                 className="resume-btn view-resume-btn"
                 rel="noopener noreferrer"
                 target="_blank"
