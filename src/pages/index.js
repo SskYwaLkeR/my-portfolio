cf10b5bf7e59c2c23a7b58699907e70b98ab3ca8
Hrishikesh Rai <hkrai39@gmail.com>
2025-02-22 03:14:44 +0530

    feat: update resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -102,7 +102,7 @@ const IndexPage = () => {
             <h2>CS graduate and javascript enthusiast.</h2>
             <div className="btn-group">
               <a
-                href="https://drive.google.com/file/d/1Po_lEwr6GiiOHw3pqCjiCADxp5WnZd43/view?usp=sharing"
+                href="https://drive.google.com/file/d/1TIe1gDM2xEdTrhj_Varpcu9N-OPuiGOi/view"
                 className="resume-btn view-resume-btn"
                 rel="noopener noreferrer"
                 target="_blank"
